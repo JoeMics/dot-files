67b8e38ddbabe5d9bfcff2b5c0e0cbee73f68294
JoeMics <josephmicla@gmail.com>
2023-04-29 11:05:51 -0700

    fix conflict between tsserver and denols

--- nvim/lua/sleepyteas/lsp.lua
+++ nvim/lua/sleepyteas/lsp.lua
@@ -1,4 +1,5 @@
 local lsp = require('lsp-zero').preset({})
+local lspconfig = require('lspconfig')
 
 lsp.on_attach(function(client, bufnr)
   lsp.default_keymaps({buffer = bufnr})
@@ -8,6 +9,32 @@ lsp.ensure_installed({
 	'tsserver',
 	'denols',
 	'volar',
+})
+
+
+lspconfig.denols.setup({
+    on_attach = on_attach,
+
+    -- to make sure ts/js files don't use this
+    root_dir = lspconfig.util.root_pattern("deno.json"),
+
+    init_options = {
+        lint = true,
+    },
+})
+
+lspconfig.tsserver.setup({
+  on_attach = on_attach,
+
+  -- to make sure deno files don't use this
+  root_dir = lspconfig.util.root_pattern("package.json"),
+
+  -- lsp will ignore single files. This means a package.json is required for all js/ts files
+  single_file_support = false,
+
+  init_options = {
+    lint = true,
+  },
 })
 
 lsp.setup()
